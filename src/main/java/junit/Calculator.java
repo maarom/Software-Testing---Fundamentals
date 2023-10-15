0d166f39405444f0e831a2c4af05403cb13b9aa1
DESKTOP-9KVA0HU\frew <makc.aromae@gmail.com>
2023-10-15 11:45:36 +0300

    adds Calculator

---
+++ src/main/java/junit/Calculator.java
@@ -0,0 +1,12 @@
+package junit;
+
+public class Calculator {
+    public int add (int x, int y) {
+        return x + y;
+    }
+
+    public int divide (int x, int y){
+        return x / y;
+    }
+}
+
